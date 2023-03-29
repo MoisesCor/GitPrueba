c79a4659eacaf4e3559e13a313aa08aad82a8142
Moises <moisesodoo@gmail.com>
2023-03-29 17:06:31 +0200

    introduccido txt

--- src/Index.java
+++ src/Index.java
@@ -2,7 +2,7 @@
 public class Index {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Este es el archivo principal, que voy a subir a mi gitHub desde git");
 
 	}
 
